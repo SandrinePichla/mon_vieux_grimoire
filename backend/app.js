c0c70e2544dc3af20cd1cf4e3dbf0f3e9f95fa30
SandrinePichla <sandrinepichla@hotmail.com>
2025-07-21 00:59:34 +0200

    Initialisation du backend avec la configuration du serveur Express et les configurations nécessaires
    - Création du package.json avec les dépendances : dotenv, express, mongoose et devDependency : nodemon.
    - Implémentation du fichier server.js pour configurer un serveur HTTP avec Express, incluant la gestion des erreurs et la normalisation des ports.

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express'); // Importation d'Express pour créer l'application
+const mongoose = require('mongoose'); // Importation de mongoose pour la connexion à MongoDB
+require('dotenv').config(); // Chargement des variables d'environnement depuis .env
+
+const app = express();
+
+// Connexion MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('✅ Connexion à MongoDB réussie'))
+.catch(error => console.error('❌ Erreur de connexion MongoDB :', error));
+
+// Middleware global
+app.use(express.json());
+
+// Middleware CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// Exemple de route test
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur Mon Vieux Grimoire API !');
+});
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const app = require('./app');
+
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+// NormalizePort renvoie un port valide, qu'il soit numéroté ou non
+const port = normalizePort(process.env.PORT ||'3000');
+app.set('port', port);
+
+// Errorhandler pour gérer les erreurs de serveur
+const errorHandler = error => {
+  if (error.syscall !== 'listen') { // Vérifie si l'erreur est liée à l'écoute
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+// Création du serveur HTTP avec l'application Express
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
